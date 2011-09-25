e3321ca6d1bfed56a55c2965093901c17f5947ec
Aaron Small <a.small@unb.ca>
2011-09-25 18:46:11 -0400

    Take command line arguments for wordlist and to set size and dimensions

--- main.c
+++ main.c
@@ -1,23 +1,75 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
+#include <unistd.h>
 #include "wordsearch.h"
 
+int dimensions = 2;
+int size = 20;
+
+void usage(char *progname)
+{
+	fprintf(stderr, "Usage: %s [options] wordlist\n", progname);
+	fprintf(stderr, "\n");
+	fprintf(stderr, "-d dim\tSpecify the number of dimensions in the puzzle\n");
+	fprintf(stderr, "-s size\tSpecify the size of each dimension\n");
+	exit(1);
+}
+
+void read_words(FILE *f, word_search *w)
+{
+	char buf[200];
+
+	while(1) {
+		// Read from stdin
+		fgets(buf, 200, f);
+		if( feof(f) ) break;
+		// Chomp newline
+		while( isspace( buf[ strlen(buf) - 1 ] ) ) buf[ strlen(buf) - 1 ] = '\0';
+		wordsearch_add( w, buf);
+	}
+}
 
 int main(int argc, char *argv[])
 {
-	char buf[200];
+	int i;
+	int c;
+	FILE *f;
 	word_search w;
 
 	// Default to 2 dimensions, 20x20
-	wordsearch_alloc( &w, 2, 20 );
-	while(1) {
-		// Read from stdin
-		fgets(buf, 200, stdin);
-		if( feof(stdin) ) break;
-		// Chomp newline
-		while( isspace( buf[ strlen(buf) - 1 ] ) ) buf[ strlen(buf) - 1 ] = '\0';
-		wordsearch_add( &w, buf);
+	while ((c = getopt(argc, argv, "d:s:h")) != -1) {
+		switch(c) {
+			case 'd':
+				dimensions = atoi(optarg);
+				break;
+			case 's':
+				size = atoi(optarg);
+				break;
+			case 'h':
+				usage(argv[0]);
+				break;
+			default:
+				break;
+		}
 	}
+
+	wordsearch_alloc( &w, dimensions, size );
+
+	if( optind < argc ) {
+		for( i = optind; i < argc; i ++ ) {
+			f = fopen(argv[i], "r");
+			if( f == NULL ) {
+				fprintf(stderr, "Failed to open %s\n", argv[i]);
+				exit(1);
+			}
+			read_words(f, &w);
+			fclose(f);
+		}
+	} else {
+		read_words(stdin, &w);
+	}
+
 	wordsearch_fit( &w );
 	wordsearch_display( &w );
 	wordsearch_solve( &w );
