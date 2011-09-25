627be01f6d70e1f4ad1dd1e410b4849a0697708f
Aaron Small <a.small@unb.ca>
2011-09-25 16:36:05 -0400

    Read words from stdin instead of hardcoding. Print out the solution to the puzzle as well.

--- main.c
+++ main.c
@@ -1,15 +1,26 @@
 #include <stdio.h>
+#include <string.h>
 #include "wordsearch.h"
 
 
 int main(int argc, char *argv[])
 {
+	char buf[200];
 	word_search w;
 
-	wordsearch_alloc( &w, 4, 5 );
-	wordsearch_add( &w, "hello" );
+	// Default to 2 dimensions, 20x20
+	wordsearch_alloc( &w, 2, 20 );
+	while(1) {
+		// Read from stdin
+		fgets(buf, 200, stdin);
+		if( feof(stdin) ) break;
+		// Chomp newline
+		while( isspace( buf[ strlen(buf) - 1 ] ) ) buf[ strlen(buf) - 1 ] = '\0';
+		wordsearch_add( &w, buf);
+	}
 	wordsearch_fit( &w );
 	wordsearch_display( &w );
+	wordsearch_solve( &w );
 	wordsearch_free( &w );
 	return 0;
 }
